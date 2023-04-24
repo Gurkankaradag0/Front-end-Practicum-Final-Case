a9f51605ccbbb0abfae7a406ad6da6dadaea07b4
gurkankaradag0 <gurkan.karadag.1907@gmail.com>
2023-04-24 22:56:26 +0300

    feat: add Film Images

---
+++ src/data/films.js
@@ -0,0 +1,28 @@
+const films = [
+    {
+        name: "A New Hope",
+        image: "https://m.media-amazon.com/images/I/91MMkv35K5L._RI_.jpg"
+    },
+    {
+        name: "The Empire Strikes Back",
+        image: "https://m.media-amazon.com/images/I/91mJqQ3gyhL._RI_.jpg"
+    },
+    {
+        name: "Return of the Jedi",
+        image: "https://m.media-amazon.com/images/I/91H37HUG4kL._RI_.jpg"
+    },
+    {
+        name: "The Phantom Menace",
+        image: "https://m.media-amazon.com/images/I/91NrqPMwWqL._RI_.jpg"
+    },
+    {
+        name: "Attack of the Clones",
+        image: "https://images-na.ssl-images-amazon.com/images/S/pv-target-images/0a6366bdd4939cb76f29829ec3c4de16f2ebea92632b0e9aca11fb009e6d659e._RI_V_TTW_.jpg"
+    },
+    {
+        name: "Revenge of the Sith",
+        image: "https://images-na.ssl-images-amazon.com/images/S/pv-target-images/3a991f1ad651779c8701339593c0a7c6afa18ba8844da38a1740e9fea2ebe873._RI_V_TTW_.jpg"
+    }
+]
+
+export default films
--- src/data/index.js
+++ src/data/index.js
@@ -1,9 +1,11 @@
 import planets from './planets'
 import vehicles from './vehicles'
+import films from './films'
 
 const types = {
     planets,
-    vehicles
+    vehicles,
+    films
 }
 
 export default types
