4967472d1468d5205c74eaf1a9e9f55c8aceee56
gurkankaradag0 <gurkan.karadag.1907@gmail.com>
2023-04-25 15:45:45 +0300

    feat: add People Images

--- src/data/index.js
+++ src/data/index.js
@@ -1,11 +1,13 @@
 import planets from './planets'
 import vehicles from './vehicles'
+import people from './people'
 import films from './films'
 import species from './species'
 
 const types = {
     planets,
     vehicles,
+    people,
     films,
     species
 }
---
+++ src/data/people.js
@@ -0,0 +1,354 @@
+import Luke_Skywalker from '~/assets/people/Luke Skywalker.png'
+import Leia_Organa from '~/assets/people/Leia Organa.png'
+import Owen_Lars from '~/assets/people/Owen Lars.png'
+import Biggs_Darklighter from '~/assets/people/Biggs Darklighter.png'
+import Obi_Wan_Kenobi from '~/assets/people/Obi-Wan Kenobi.png'
+import Anakin_Skywalker from '~/assets/people/Anakin Skywalker.png'
+import Wilhuff_Tarkin from '~/assets/people/Wilhuff Tarkin.png'
+import Jek_Tono_Porkins from '~/assets/people/Jek Tono Porkins.png'
+import Bossk from '~/assets/people/Bossk.png'
+import Arvel_Crynyd from '~/assets/people/Arvel Crynyd.png'
+import Padmé_Amidala from '~/assets/people/Padmé Amidala.png'
+import Ric_Olié from '~/assets/people/Ric Olié.png'
+import Dud_Bolt from '~/assets/people/Dud Bolt.png'
+import Cordé from '~/assets/people/Cordé.png'
+import Barriss_Offee from '~/assets/people/Barriss Offee.png'
+import Dooku from '~/assets/people/Dooku.png'
+import Bail_Prestor_Organa from '~/assets/people/Bail Prestor Organa.png'
+import Lama_Su from '~/assets/people/Lama Su.png'
+import Taun_We from '~/assets/people/Taun We.png'
+import Jocasta_Nu from '~/assets/people/Jocasta Nu.png'
+import Grievous from '~/assets/people/Grievous.png'
+
+const people = [
+    {
+        name: "Luke Skywalker",
+        image: Luke_Skywalker
+    },
+    {
+        name: "C-3PO",
+        image: "https://static.wikia.nocookie.net/starwars/images/0/02/C3PO-TROSposter.png"
+    },
+    {
+        name: "R2-D2",
+        image: "https://static.wikia.nocookie.net/starwars/images/e/eb/ArtooTFA2-Fathead.png"
+    },
+    {
+        name: "Darth Vader",
+        image: "https://static.wikia.nocookie.net/villains/images/0/0f/Darthvader3.png"
+    },
+    {
+        name: "Leia Organa",
+        image: Leia_Organa
+    },
+    {
+        name: "Owen Lars",
+        image: Owen_Lars
+    },
+    {
+        name: "Beru Whitesun lars",
+        image: "https://static.wikia.nocookie.net/ipdkverse/images/0/0a/Beru_Lars_1.png"
+    },
+    {
+        name: "R5-D4",
+        image: "https://static.wikia.nocookie.net/starwars/images/3/3f/R5D4-AG.png"
+    },
+    {
+        name: "Biggs Darklighter",
+        image: Biggs_Darklighter
+    },
+    {
+        name: "Obi-Wan Kenobi",
+        image: Obi_Wan_Kenobi
+    },
+    {
+        name: "Anakin Skywalker",
+        image: Anakin_Skywalker
+    },
+    {
+        name: "Wilhuff Tarkin",
+        image: Wilhuff_Tarkin
+    },
+    {
+        name: "Chewbacca",
+        image: "https://static.wikia.nocookie.net/starwars/images/4/48/Chewbacca_TLJ.png"
+    },
+    {
+        name: "Han Solo",
+        image: "https://static.wikia.nocookie.net/star-wars-canon/images/6/63/TFA_Han_Solo_Render.png"
+    },
+    {
+        name: "Greedo",
+        image: "https://static.wikia.nocookie.net/theclonewiki/images/6/63/Greedo_detail.png"
+    },
+    {
+        name: "Jabba Desilijic Tiure",
+        image: "https://static.wikia.nocookie.net/starwars/images/7/7f/Jabba_SWSB.png"
+    },
+    {
+        name: "Wedge Antilles",
+        image: "https://static.wikia.nocookie.net/p__/images/6/61/Dbvg.png/revision/latest/scale-to-width-down/246?cb=20200207002729&path-prefix=protagonist"
+    },
+    {
+        name: "Jek Tono Porkins",
+        image: Jek_Tono_Porkins
+    },
+    {
+        name: "Yoda",
+        image: "https://static.wikia.nocookie.net/yildizsavaslari/images/c/c3/Yoda_TPM_RotS.png/revision/latest/scale-to-width-down/350?cb=20201018024212&path-prefix=tr"
+    },
+    {
+        name: "Palpatine",
+        image: "https://static.wikia.nocookie.net/starwars/images/e/e2/Palpatine-CEUEEd.png"
+    },
+    {
+        name: "Boba Fett",
+        image: "https://static.wikia.nocookie.net/yildizsavaslari/images/9/9e/BobaFettRepaintedArmor-SS.png/revision/latest/scale-to-width-down/302?cb=20211208183954&path-prefix=tr"
+    },
+    {
+        name: "IG-88",
+        image: "https://static.wikia.nocookie.net/starwars/images/3/3f/IG-88_Sideshow.png"
+    },
+    {
+        name: "Bossk",
+        image: Bossk
+    },
+    {
+        name: "Lando Calrissian",
+        image: "https://milnersblog.files.wordpress.com/2018/04/characters-of-solo-a-star-wars-story-lando-calrissian-transparent-background-png.png"
+    },
+    {
+        name: "Lobot",
+        image: "https://static.wikia.nocookie.net/starwars/images/e/e0/Lobot_SWSB.png"
+    },
+    {
+        name: "Ackbar",
+        image: "https://static.wikia.nocookie.net/starwars/images/2/29/Admiral_Ackbar_RH.png"
+    },
+    {
+        name: "Mon Mothma",
+        image: "https://static.wikia.nocookie.net/star-wars-canon/images/b/bf/Mon-mothma_sith.png"
+    },
+    {
+        name: "Arvel Crynyd",
+        image: Arvel_Crynyd
+    },
+    {
+        name: "Wicket Systri Warrick",
+        image: "https://static.wikia.nocookie.net/p__/images/4/45/Wicket_detail.png/revision/latest/scale-to-width-down/296?cb=20181117182827&path-prefix=protagonist"
+    },
+    {
+        name: "Nien Nunb",
+        image: "https://static.wikia.nocookie.net/starwars/images/1/14/Old_nien_nunb_-_profile.png"
+    },
+    {
+        name: "Qui-Gon Jinn",
+        image: "https://static.wikia.nocookie.net/star-wars-canon/images/a/ad/Qui-Gon_Jinn.png"
+    },
+    {
+        name: "Nute Gunray",
+        image: "https://static.wikia.nocookie.net/yildizsavaslari/images/f/fd/Nute_Gunray_SWE.png/revision/latest/scale-to-width-down/350?cb=20210412212337&path-prefix=tr"
+    },
+    {
+        name: "Finis Valorum",
+        image: "https://static.wikia.nocookie.net/yildizsavaslari/images/5/51/ValorumPortrait-SWE.png/revision/latest/scale-to-width-down/350?cb=20210524140815&path-prefix=tr"
+    },
+    {
+        name: "Padmé Amidala",
+        image: Padmé_Amidala
+    },
+    {
+        name: "Jar Jar Binks",
+        image: "https://static.wikia.nocookie.net/p__/images/6/69/Jar_Jar_Binks.png/revision/latest/scale-to-width-down/206?cb=20190514062237&path-prefix=protagonist"
+    },
+    {
+        name: "Roos Tarpals",
+        image: "https://static.wikia.nocookie.net/starwars/images/c/c8/Tarpals-FFp47.png"
+    },
+    {
+        name: "Rugor Nass",
+        image: "https://static.wikia.nocookie.net/starwars/images/6/63/BossNass-SWCT.png"
+    },
+    {
+        name: "Ric Olié",
+        image: Ric_Olié
+    },
+    {
+        name: "Watto",
+        image: "https://static.wikia.nocookie.net/starwars/images/1/1b/Watto-SWE.png"
+    },
+    {
+        name: "Sebulba",
+        image: "https://static.wikia.nocookie.net/villainous-benchmark/images/3/3f/Sebulba_VB.png"
+    },
+    {
+        name: "Quarsh Panaka",
+        image: "https://static.wikia.nocookie.net/starwars/images/a/a3/MoffPanaka-FFp34.png"
+    },
+    {
+        name: "Shmi Skywalker",
+        image: "https://static.wikia.nocookie.net/starwars/images/1/17/Shmi_FF.png"
+    },
+    {
+        name: "Darth Maul",
+        image: "https://static.wikia.nocookie.net/theclonewiki/images/0/0e/Darthmaul_detail-4.png"
+    },
+    {
+        name: "Bib Fortuna",
+        image: "https://static.wikia.nocookie.net/starwars/images/1/12/BibFortuna-CEUEEd.png"
+    },
+    {
+        name: "Ayla Secura",
+        image: "https://static.wikia.nocookie.net/starwars/images/4/4d/Aayla_Secura_SWE.png"
+    },
+    {
+        name: "Ratts Tyerel",
+        image: "https://static.wikia.nocookie.net/starwars/images/a/a5/RattsHS-SWE.png"
+    },
+    {
+        name: "Dud Bolt",
+        image: Dud_Bolt
+    },
+    {
+        name: "Gasgano",
+        image: "https://static.wikia.nocookie.net/starwars/images/a/a4/GasganoHS-SWE.png"
+    },
+    {
+        name: "Ben Quadinaros",
+        image: "https://static.wikia.nocookie.net/starwars/images/0/02/BenQuadinarosHS-SWE.png"
+    },
+    {
+        name: "Mace Windu",
+        image: "https://static.wikia.nocookie.net/starwars/images/2/27/MaceWindu_-WoSW.png"
+    },
+    {
+        name: "Ki-Adi-Mundi",
+        image: "https://static.wikia.nocookie.net/yildizsavaslari/images/6/6d/KiAdiMundi-SWCT.png/revision/latest/scale-to-width-down/350?cb=20211028160454&path-prefix=tr"
+    },
+    {
+        name: "Kit Fisto",
+        image: "https://static.wikia.nocookie.net/starwars/images/2/25/KitFisto-USWNE.png"
+    },
+    {
+        name: "Eeth Koth",
+        image: "https://static.wikia.nocookie.net/yildizsavaslari/images/4/4e/EethKothCardTrader.png/revision/latest/scale-to-width-down/350?cb=20211003134755&path-prefix=tr"
+    },
+    {
+        name: "Adi Gallia",
+        image: "https://static.wikia.nocookie.net/starwars/images/f/f2/AdiGallia2-SWE.png"
+    },
+    {
+        name: "Saesee Tiin",
+        image: "https://static.wikia.nocookie.net/starwars/images/a/a3/SaeseeTiin-SWCT.png"
+    },
+    {
+        name: "Yarael Poof",
+        image: "https://static.wikia.nocookie.net/starwars/images/b/b2/YaraelPoof-SWCT.png"
+    },
+    {
+        name: "Plo Koon",
+        image: "https://static.wikia.nocookie.net/starwars/images/b/bf/PloKoonCardTrader.png"
+    },
+    {
+        name: "Mas Amedda",
+        image: "https://static.wikia.nocookie.net/starwars/images/3/37/Mas_Amedda_SWCT.png"
+    },
+    {
+        name: "Gregar Typho",
+        image: "https://static.wikia.nocookie.net/starwars/images/7/78/Typho_TCW.png"
+    },
+    {
+        name: "Cordé",
+        image: Cordé
+    },
+    {
+        name: "Cliegg Lars",
+        image: "https://static.wikia.nocookie.net/starwars/images/5/58/ClieggLarsDetail-SWE.png"
+    },
+    {
+        name: "Poggle the Lesser",
+        image: "https://static.wikia.nocookie.net/starwars/images/9/93/Poggle_the_lesser_-_sw_card_trader.png"
+    },
+    {
+        name: "Luminara Unduli",
+        image: "https://static.wikia.nocookie.net/yildizsavaslari/images/2/21/LuminaraHS-SWE_%28new%29.png/revision/latest/scale-to-width-down/350?cb=20210121073931&path-prefix=tr"
+    },
+    {
+        name: "Barriss Offee",
+        image: Barriss_Offee
+    },
+    {
+        name: "Dormé",
+        image: "https://static.wikia.nocookie.net/starwars/images/7/72/Dorme-DaG.png"
+    },
+    {
+        name: "Dooku",
+        image: Dooku
+    },
+    {
+        name: "Bail Prestor Organa",
+        image: Bail_Prestor_Organa
+    },
+    {
+        name: "Jango Fett",
+        image: "https://static.wikia.nocookie.net/starwars/images/5/51/JangoFett-OP.png"
+    },
+    {
+        name: "Zam Wesell",
+        image: "https://static.wikia.nocookie.net/starwars/images/9/9a/ZamWesell-FF.png"
+    },
+    {
+        name: "Dexter Jettster",
+        image: "https://static.wikia.nocookie.net/starwars/images/1/1c/DexterHS-SWE.png"
+    },
+    {
+        name: "Lama Su",
+        image: Lama_Su
+    },
+    {
+        name: "Taun We",
+        image: Taun_We
+    },
+    {
+        name: "Jocasta Nu",
+        image: Jocasta_Nu
+    },
+    {
+        name: "R4-P17",
+        image: "https://static.wikia.nocookie.net/starwars/images/5/52/R4-P17_USWNE.png"
+    },
+    {
+        name: "Wat Tambor",
+        image: "https://static.wikia.nocookie.net/villains/images/a/a5/Wat_Tambor.png"
+    },
+    {
+        name: "San Hill",
+        image: "https://static.wikia.nocookie.net/starwars/images/6/63/SanHill-BaseSeries5.png"
+    },
+    {
+        name: "Shaak Ti",
+        image: "https://static.wikia.nocookie.net/starwars/images/2/20/Shaak_Ti_closeup-SWE.png"
+    },
+    {
+        name: "Grievous",
+        image: Grievous
+    },
+    {
+        name: "Tarfful",
+        image: "https://us.v-cdn.net/6025736/uploads/editor/vn/ivn84ctppet7.png"
+    },
+    {
+        name: "Raymus Antilles",
+        image: "https://static.wikia.nocookie.net/starwars/images/8/82/RaymusAntilles-FFp46.png"
+    },
+    {
+        name: "Sly Moore",
+        image: "https://static.wikia.nocookie.net/starwars/images/b/b7/SlyMooreStare-OP.png"
+    },
+    {
+        name: "Tion Medon",
+        image: "https://static.wikia.nocookie.net/starwars/images/1/16/TionMedon-SWI90.png"
+    }
+]
+
+export default people
