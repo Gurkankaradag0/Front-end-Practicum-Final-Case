a8572396119929b0a2c3b988ddd56fffc3588e78
gurkankaradag0 <gurkan.karadag.1907@gmail.com>
2023-04-25 18:36:26 +0300

    feat: restore store

--- src/services/api.js
+++ src/services/api.js
@@ -18,7 +18,22 @@ const getPlanets = async (page = 1) => {
             const url = element.url.split('/')
             newData.results[index].id = parseInt(url[url.length - 2])
             newData.results[index].image = imageData.planets.find(planet => planet.name === element.name)?.image
-        });
+
+            const residents = []
+            element.residents.forEach(resident => {
+                const url = resident.split('/')
+                residents.push(parseInt(url[url.length - 2]))
+            })
+
+            const films = []
+            element.films.forEach(film => {
+                const url = film.split('/')
+                films.push(parseInt(url[url.length - 2]))
+            })
+
+            newData.results[index].residents = residents
+            newData.results[index].films = films
+        })
         setPlanetsNextPage(newData.next)
         addPlanets(newData.results.filter(result => result.name !== "unknown"))
         return true
@@ -38,7 +53,22 @@ const getShips = async (page = 1) => {
             const url = element.url.split('/')
             newData.results[index].id = parseInt(url[url.length - 2])
             newData.results[index].image = imageData.ships.find(ship => ship.name === element.name)?.image
-        });
+
+            const pilots = []
+            element.pilots.forEach(pilot => {
+                const url = pilot.split('/')
+                pilots.push(parseInt(url[url.length - 2]))
+            })
+
+            const films = []
+            element.films.forEach(film => {
+                const url = film.split('/')
+                films.push(parseInt(url[url.length - 2]))
+            })
+
+            newData.results[index].pilots = pilots
+            newData.results[index].films = films
+        })
         setShipsNextPage(newData.next)
         addShips(newData.results)
         return true
@@ -58,7 +88,22 @@ const getVehicles = async (page = 1) => {
             const url = element.url.split('/')
             newData.results[index].id = parseInt(url[url.length - 2])
             newData.results[index].image = imageData.vehicles.find(vehicle => vehicle.name === element.name)?.image
-        });
+
+            const pilots = []
+            element.pilots.forEach(pilot => {
+                const url = pilot.split('/')
+                pilots.push(parseInt(url[url.length - 2]))
+            })
+
+            const films = []
+            element.films.forEach(film => {
+                const url = film.split('/')
+                films.push(parseInt(url[url.length - 2]))
+            })
+
+            newData.results[index].pilots = pilots
+            newData.results[index].films = films
+        })
         setVehiclesNextPage(newData.next)
         addVehicles(newData.results)
         return true
@@ -78,7 +123,39 @@ const getCharacters = async (page = 1) => {
             const url = element.url.split('/')
             newData.results[index].id = parseInt(url[url.length - 2])
             newData.results[index].image = imageData.characters.find(character => character.name === element.name)?.image
-        });
+
+            const homeworld = element.homeworld.split('/')
+
+            const films = []
+            element.films.forEach(film => {
+                const url = film.split('/')
+                films.push(parseInt(url[url.length - 2]))
+            })
+
+            const species = []
+            element.species.forEach(race => {
+                const url = race.split('/')
+                species.push(parseInt(url[url.length - 2]))
+            })
+
+            const vehicles = []
+            element.vehicles.forEach(vehicle => {
+                const url = vehicle.split('/')
+                vehicles.push(parseInt(url[url.length - 2]))
+            })
+
+            const starships = []
+            element.starships.forEach(ship => {
+                const url = ship.split('/')
+                starships.push(parseInt(url[url.length - 2]))
+            })
+
+            newData.results[index].homeworld = parseInt(homeworld[homeworld.length - 2])
+            newData.results[index].species = species
+            newData.results[index].films = films
+            newData.results[index].vehicles = vehicles
+            newData.results[index].starships = starships
+        })
         setCharactersNextPage(newData.next)
         addCharacters(newData.results)
         return true
@@ -98,7 +175,43 @@ const getFilms = async (page = 1) => {
             const url = element.url.split('/')
             newData.results[index].id = parseInt(url[url.length - 2])
             newData.results[index].image = imageData.films.find(film => film.name === element.title)?.image
-        });
+
+            const characters = []
+            element.characters.forEach(character => {
+                const url = character.split('/')
+                characters.push(parseInt(url[url.length - 2]))
+            })
+
+            const planets = []
+            element.planets.forEach(planet => {
+                const url = planet.split('/')
+                planets.push(parseInt(url[url.length - 2]))
+            })
+
+            const species = []
+            element.species.forEach(race => {
+                const url = race.split('/')
+                species.push(parseInt(url[url.length - 2]))
+            })
+
+            const vehicles = []
+            element.vehicles.forEach(vehicle => {
+                const url = vehicle.split('/')
+                vehicles.push(parseInt(url[url.length - 2]))
+            })
+
+            const starships = []
+            element.starships.forEach(ship => {
+                const url = ship.split('/')
+                starships.push(parseInt(url[url.length - 2]))
+            })
+
+            newData.results[index].species = species
+            newData.results[index].characters = characters
+            newData.results[index].planets = planets
+            newData.results[index].vehicles = vehicles
+            newData.results[index].starships = starships
+        })
         setFilmsNextPage(newData.next)
         addFilms(newData.results)
         return true
@@ -118,7 +231,25 @@ const getRaces = async (page = 1) => {
             const url = element.url.split('/')
             newData.results[index].id = parseInt(url[url.length - 2])
             newData.results[index].image = imageData.races.find(race => race.name === element.name)?.image
-        });
+
+            const homeworld = element.homeworld.split('/')
+
+            const films = []
+            element.films.forEach(film => {
+                const url = film.split('/')
+                films.push(parseInt(url[url.length - 2]))
+            })
+
+            const characters = []
+            element.characters.forEach(character => {
+                const url = character.split('/')
+                characters.push(parseInt(url[url.length - 2]))
+            })
+
+            newData.results[index].homeworld = parseInt(homeworld[homeworld.length - 2])
+            newData.results[index].characters = characters
+            newData.results[index].films = films
+        })
         setRacesNextPage(newData.next)
         addRaces(newData.results)
         return true
