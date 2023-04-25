463557c09ff1d64cc97ad613e324d57152368c1d
gurkankaradag0 <gurkan.karadag.1907@gmail.com>
2023-04-25 19:25:11 +0300

    feat: add custom hook for Detail Page

--- src/utils/store.js
+++ src/utils/store.js
@@ -15,9 +15,11 @@ export const useNavbar = () => useSelector(state => state.navbar)
 export const setOpen = () => store.dispatch(_setOpen())
 export const setClose = () => store.dispatch(_setClose())
 
+// Detail Hook
+export const useDetail = (category, id) => useSelector(state => state[category].results.find(result => result.id === id))
+
 // Planets
 export const usePlanets = () => useSelector(state => state.planets)
-export const usePlanet = id => useSelector(state => state.planets.results.filter(planet => planet.id === id)[0])
 export const addPlanets = planets => store.dispatch(_addPlanets(planets))
 
 export const setPlanetsNextPage = page => store.dispatch(_setPlanetsNextPage(page))
@@ -25,7 +27,6 @@ export const setPlanetsHasMore = hasMore => store.dispatch(_setPlanetsHasMore(ha
 
 // Ships
 export const useShips = () => useSelector(state => state.ships)
-export const useSpaceShip = id => useSelector(state => state.ships.results.filter(ship => ship.id === id)[0])
 export const addShips = ships => store.dispatch(_addShips(ships))
 
 export const setShipsNextPage = page => store.dispatch(_setShipsNextPage(page))
@@ -33,7 +34,6 @@ export const setShipsHasMore = hasMore => store.dispatch(_setShipsHasMore(hasMor
 
 // Vehicles
 export const useVehicles = () => useSelector(state => state.vehicles)
-export const useVehicle = id => useSelector(state => state.vehicles.results.filter(vehicle => vehicle.id === id)[0])
 export const addVehicles = vehicles => store.dispatch(_addVehicles(vehicles))
 
 export const setVehiclesNextPage = page => store.dispatch(_setVehiclesNextPage(page))
@@ -41,7 +41,6 @@ export const setVehiclesHasMore = hasMore => store.dispatch(_setVehiclesHasMore(
 
 // Characters
 export const useCharacters = () => useSelector(state => state.characters)
-export const usePerson = id => useSelector(state => state.characters.results.filter(character => character.id === id)[0])
 export const addCharacters = characters => store.dispatch(_addCharacters(characters))
 
 export const setCharactersNextPage = page => store.dispatch(_setCharactersNextPage(page))
@@ -49,7 +48,6 @@ export const setCharactersHasMore = hasMore => store.dispatch(_setCharactersHasM
 
 // Films
 export const useFilms = () => useSelector(state => state.films)
-export const useFilm = id => useSelector(state => state.films.results.filter(film => film.id === id)[0])
 export const addFilms = films => store.dispatch(_addFilms(films))
 
 export const setFilmsNextPage = page => store.dispatch(_setFilmsNextPage(page))
@@ -57,7 +55,6 @@ export const setFilmsHasMore = hasMore => store.dispatch(_setFilmsHasMore(hasMor
 
 // Races
 export const useRaces = () => useSelector(state => state.races)
-export const useRaces_ = id => useSelector(state => state.races.results.filter(race => race.id === id)[0])
 export const addRaces = races => store.dispatch(_addRaces(races))
 
 export const setRacesNextPage = page => store.dispatch(_setRacesNextPage(page))
